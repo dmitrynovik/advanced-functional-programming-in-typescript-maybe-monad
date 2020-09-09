6315377c318996e39167bc9824fa87fe335bcaf5
Dmitry Novik <dmitry.novik@gmail.com>
2020-09-09 11:33:50 +1000

    non-functional version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeRepository } from "./employee.repository";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'advanced-functional-programming-in-typescript-maybe-monad';
+  repository: EmployeeRepository;
+
+  ngOnInit(): void {
+    const employeeIdInputEl = document.getElementById("employeeIdInput") as HTMLInputElement;
+    const findEmployeeButtonEl = document.getElementById("findEmployeeButton");
+    const searchResultsEl = document.getElementById("searchResults");
+
+    this.repository = new EmployeeRepository();
+
+    findEmployeeButtonEl.addEventListener("click", () => {
+      const supervisorName = this.getSupervisorName(employeeIdInputEl.value);
+      if (supervisorName) {
+          searchResultsEl.innerText = `Supervisor name: ${supervisorName}`;
+      } else {
+          searchResultsEl.innerText = "Could not find supervisor for given id";
+      }
+  });
 }
+
+  getSupervisorName(enteredId: string) {
+      if (enteredId) {
+          const employee = this.repository.findById(parseInt(enteredId));
+          if (employee && employee.supervisorId) {
+              const supervisor = this.repository.findById(employee.supervisorId);
+              if (supervisor) {
+                  return supervisor.name;
+              }
+          }
+      }
+  }   
+}
+

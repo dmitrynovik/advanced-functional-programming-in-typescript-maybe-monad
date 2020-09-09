67c80f7c06dbc5f324c098004ad4790df917fdb8
Dmitry Novik <dmitry.novik@gmail.com>
2020-09-09 12:14:16 +1000

    minor: use fat arrow functions

--- src/app/maybe.ts
+++ src/app/maybe.ts
@@ -1,9 +1,9 @@
 export class Maybe<T> {
-    private constructor(private value: T | null) {}
+    private constructor(private value: T | null) {  }
 
     static some<T>(value: T) {
         if (!value) {
-            throw Error("Provided value must not be empty");
+            throw Error('Provided value must not be empty');
         }
         return new Maybe(value);
     }
@@ -16,9 +16,7 @@ export class Maybe<T> {
         return value ? Maybe.some(value) : Maybe.none<T>();
     }
 
-    getOrElse(defaultValue: T) {
-        return this.value === null ? defaultValue : this.value;
-    }
+    getOrElse = (defaultValue: T) => this.value === null ? defaultValue : this.value;
 
     map<R>(f: (wrapped: T) => R): Maybe<R> {
         if (this.value === null) {
@@ -27,7 +25,7 @@ export class Maybe<T> {
             return Maybe.fromValue(f(this.value));
         }
     }
-    
+
     flatMap<R>(f: (wrapped: T) => Maybe<R>): Maybe<R> {
         if (this.value === null) {
             return Maybe.none<R>();
